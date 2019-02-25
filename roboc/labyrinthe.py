83e2e2f74df8710589e412143e646e09d169f6e2
jerev7 <verdet.jeremie@gmail.com>
2019-02-25 17:19:44 +0100

    ajouté la position du robot avec robotX et robotY dans le constructeur

--- roboc/labyrinthe.py
+++ roboc/labyrinthe.py
@@ -7,19 +7,33 @@ class Labyrinthe:
 	"""Classe représentant un labyrinthe."""
 
 	def __init__(self, robot, grille):
-		self.robot = robot
+		for numero_ligne, ligne in enumerate(self.grille):
+			if "X" in ligne:
+				self.robotY = numero_ligne
+				ligne_liste = list(ligne)
+				self.robotX = ligne_liste.index("X")
 		self.grille = grille
 	
 	def afficher_carte(self):
-		print(self.grille) 
+		for numero_ligne, ligne in enumerate(self.grille):
+			if numero_ligne == self.robotY:
+				ligne_liste = list(ligne)
+				position_robot = self.robotX
+				ligne_liste[position_robot] = "Y"
+				nouvelle_ligne = "".join(ligne_liste)
+				print(nouvelle_ligne)
+
+
+			else:
+				print(ligne)	
+
 		
 	def deplacement(self):
 
 		direction = input("choisissez votre direction puis appuyez sur 'Entrée' : \n")
 		nbre_cases = input("choisissez le nombre de cases puis appuyez sur 'Entrée' : \n")
 		direction = direction.lower()
-		for element in self.grille:
-			print(element)
+		self.afficher_carte()
 		print(f"{direction}{nbre_cases}")
 		
 		if nbre_cases == "":
@@ -91,5 +105,4 @@ class Labyrinthe:
 				i += 1
 		
 		# On affiche les lignes modifié avec la nouvelle position de X		
-		for element in self.grille:
-			print(element)
+		self.afficher_carte()
